913c8e1d5be5ab97aa78323f4fe33fff1c87defc
mainurrahat <mainurrahat51@gmail.com>
2025-08-12 00:00:11 +0600

    Add factorial function

--- basic_program.c
+++ basic_program.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
-
+int factorial(int n)
+{
+    if (n <= 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
 int main()
 {
     printf("Hello World\n");
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    int result = factorial(num);
+    printf("Factorial of %d is %d\n", num, result);
     return 0;
 }
