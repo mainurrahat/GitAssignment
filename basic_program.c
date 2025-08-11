88c6a0afc68c1c7eaf25d017756205a4f4171ca9
mainurrahat <mainurrahat51@gmail.com>
2025-08-12 00:00:11 +0600

    Add nth prime number function

--- basic_program.c
+++ basic_program.c
@@ -13,6 +13,21 @@ int fibonacci(int n)
     else
         return fibonacci(n - 1) + fibonacci(n - 2);
 }
+int isPrime(int n) {
+    if(n <= 1) return 0;
+    for(int i = 2; i*i <= n; i++)
+        if(n % i == 0) return 0;
+    return 1;
+}
+
+int nthPrime(int n) {
+    int count = 0, num = 2;
+    while(1) {
+        if(isPrime(num)) count++;
+        if(count == n) return num;
+        num++;
+    }
+}
 
 int main()
 {
@@ -24,6 +39,9 @@ int main()
     printf("Factorial of %d is %d\n", num, result);
     result = fibonacci(num);
     printf("Fibonacci of %d is %d\n", num, result);
+    result = nthPrime(num);
+    printf("The %d-th prime number is %d\n", num, result);
+    
 
     return 0;
 }
